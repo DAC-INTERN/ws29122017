b1ee0c6c1fa109e781e89942675e8cd60ec19a1b
Wolf <cgvnd123@gmail.com>
2018-01-02 16:53:21 +0700

    update css

--- rss.php
+++ rss.php
@@ -5,26 +5,19 @@
  * Date: 29/12/2017
  * Time: 10:16
  */
-
 require __DIR__ . '/vendor/autoload.php';
-
 header('Access-Control-Allow-Origin: *');
 use JC\HttpClient\JCRequest;
-
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
 }else{
     die;
 }
-
 $response = JCRequest::get($url);
-
 $xmlString = $response->body();
 $xmlObject = simplexml_load_string($xmlString);
 $data = [];
-
 //var_dump($xmlObject);
-
 foreach ($xmlObject->channel->item as $object) {
     $data[] = [
         'title' => (string)$object->title,
@@ -34,3 +27,5 @@ foreach ($xmlObject->channel->item as $object) {
 }
 
 echo json_encode($data);
+
+?>
